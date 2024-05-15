05c737faaa173f4311d7c98b1c5d321098fc64ad
zheng.gong <zheng.gong@centricsoftware.com>
2024-05-15 17:02:46 +0800

    init

---
+++ src/test/java/com/neko/springbootneko/SpringBootNekoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neko.springbootneko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNekoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
