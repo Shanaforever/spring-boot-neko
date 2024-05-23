7e9566122f3f237601b1cd7c99181192ffd91356
zheng.gong <zheng.gong@centricsoftware.com>
2024-05-23 12:23:54 +0800

    feat: update pom dependencies
    build dependency, the server can run successfuly now

---
+++ neko-server/src/main/java/com/neko/boot/server/NekoServerApplication.java
@@ -0,0 +1,15 @@
+package com.neko.boot.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Gong Zheng
+ * @since 2024/5/23 12:21
+ */
+@SpringBootApplication
+public class NekoServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NekoServerApplication.class);
+    }
+}
